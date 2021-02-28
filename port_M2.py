b26f50499245730de2ed7d83bad4d907014fc37a
paulbqnt <68791915+paulbqnt@users.noreply.github.com>
2021-02-28 22:48:57 +0100

    Add files via upload

--- port_M2.py
+++ port_M2.py
@@ -13,11 +13,6 @@ new_liste = []
 for i in liste_etf:
     new_liste.append(i.lower().replace(" ", "_"))
 
-
-    
-    
-    
-
 # Creation des Data Frames
 for etf in liste_etf:
     vars()[etf.lower().replace(" ", "_")] = pd.DataFrame(
@@ -36,30 +31,14 @@ for stock in liste_stock:
 
 new_liste = ['ishares_us_financials', 'ishares_us_telecommunications', 'ishares_us_industrials', 'ishares_us_consumer_services', 'ishares_us_consumer_goods', 'ishares_us_utilities', 'ishares_us_basic_materials', 'ishares_us_energy', "tsla", "aapl", "jnj"]
 
-
-
 # Conversion du format date des Data Frames
 for item in new_liste:
     exec(f"{item}.index = {item}.index.strftime('%d/%m/%Y')")
     
-
 writer = pd.ExcelWriter("portfolio.xlsx", engine = "xlsxwriter")
-
-
-# Excel ws name must be <= 31 characters
-# itteration dans la liste pour retirer les 11 premiers caracteres
-#short_liste = [e[:16] for e in new_liste]
-
-
-
 
 # Creation du fichier excel dans le working directory du fichier port_M2.py
 for item in new_liste:
-    #for x in short_liste:
     exec(f"{item}.to_excel(writer, sheet_name='{item}')")
 
 writer.save()
-
-
-
-
