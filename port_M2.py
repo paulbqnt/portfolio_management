1dc000611abf90d0118f505dfd55d2711b61a884
paulbqnt <68791915+paulbqnt@users.noreply.github.com>
2021-02-28 22:44:01 +0100

    Add files via upload

--- port_M2.py
+++ port_M2.py
@@ -21,7 +21,7 @@ for i in liste_etf:
 # Creation des Data Frames
 for etf in liste_etf:
     vars()[etf.lower().replace(" ", "_")] = pd.DataFrame(
-        investpy.etfs.get_etf_historical_data(etf, "united states", "01/01/2008","01/01/2021", 
+        investpy.etfs.get_etf_historical_data(etf, "united states", "01/01/2009","01/01/2021", 
                                               stock_exchange=None, 
                                               as_json=False, 
                                               order="ascending",
@@ -29,13 +29,12 @@ for etf in liste_etf:
     
 for stock in liste_stock:
     vars()[stock.lower()] = pd.DataFrame(
-        investpy.stocks.get_stock_historical_data(stock, "united states", "01/01/2008", "01/01/2021",
+        investpy.stocks.get_stock_historical_data(stock, "united states", "01/01/2009", "01/01/2021",
                                                   as_json=False,
                                                   order="ascending",
                                                   interval="Daily"))
 
-new_liste = ('ishares_us_financials', 'ishares_us_telecommunications', 'ishares_us_industrials', 'ishares_us_consumer_services',
-             'ishares_us_consumer_goods', 'ishares_us_utilities', 'ishares_us_basic_materials', 'ishares_us_energy', "tsla", "aapl", "jnj")
+new_liste = ['ishares_us_financials', 'ishares_us_telecommunications', 'ishares_us_industrials', 'ishares_us_consumer_services', 'ishares_us_consumer_goods', 'ishares_us_utilities', 'ishares_us_basic_materials', 'ishares_us_energy', "tsla", "aapl", "jnj"]
 
 
 
@@ -44,17 +43,20 @@ for item in new_liste:
     exec(f"{item}.index = {item}.index.strftime('%d/%m/%Y')")
     
 
-writer = pd.ExcelWriter("pandas_multiple.xlsx", engine = "xlsxwriter")
+writer = pd.ExcelWriter("portfolio.xlsx", engine = "xlsxwriter")
 
 
 # Excel ws name must be <= 31 characters
 # itteration dans la liste pour retirer les 11 premiers caracteres
-short_liste = [e[:16] for e in new_liste]
+#short_liste = [e[:16] for e in new_liste]
+
+
+
 
 # Creation du fichier excel dans le working directory du fichier port_M2.py
 for item in new_liste:
-    for x in short_liste:
-        exec(f"{item}.to_excel(writer, sheet_name='{x}.excel')")
+    #for x in short_liste:
+    exec(f"{item}.to_excel(writer, sheet_name='{item}')")
 
 writer.save()
 
